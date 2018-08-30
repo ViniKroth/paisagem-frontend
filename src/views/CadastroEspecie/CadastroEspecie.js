c9937e3565c3d73c8133429df0564a0931a2e27f
Rodrigo.rosa <rodrigo.rosa.005@acad.pucrs.br>
2018-08-29 22:04:43 -0300

    criada pagina inicial de cadastro de uma nova especie(divido em 4 etapas)

--- src/views/CadastroEspecie/CadastroEspecie.js
+++ src/views/CadastroEspecie/CadastroEspecie.js
@@ -1,18 +1,151 @@
-import React from 'react'
-import "./style.css"
-import ImageComponent from "components/UploadImg/ImageComponent.js"
-//views
-import Page from 'views/Page';
+import React from 'react';
+import PropTypes from 'prop-types';
+import withStyles from '@material-ui/core/styles/withStyles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Paper from '@material-ui/core/Paper';
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
-class CadastroEspecie extends Page {
-	render() {
-		return (
-			<div>
-				<p>cadastro espécie</p>
-				<ImageComponent/>
-			</div>
-		)
-	}
+import DadosBasicosForm from "components/CadastroEspecie/formDados.js"
+
+const styles = theme => ({
+  appBar: {
+    position: 'relative',
+  },
+  layout: {
+    width: 'auto',
+    marginLeft: theme.spacing.unit * 2,
+    marginRight: theme.spacing.unit * 2,
+    [theme.breakpoints.up(900 + theme.spacing.unit * 2 * 2)]: {
+      width: 800,
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    },
+  },
+  paper: {
+    marginTop: theme.spacing.unit * 3,
+    marginBottom: theme.spacing.unit * 3,
+    padding: theme.spacing.unit * 2,
+    [theme.breakpoints.up(600 + theme.spacing.unit * 3 * 2)]: {
+      marginTop: theme.spacing.unit * 3,
+      marginBottom: theme.spacing.unit * 3,
+	  padding: theme.spacing.unit * 3,
+	  
+    },
+  },
+  stepper: {
+    padding: `${theme.spacing.unit * 3}px 0 ${theme.spacing.unit * 5}px`,
+  },
+  buttons: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+  },
+  button: {
+    marginTop: theme.spacing.unit * 3,
+    marginLeft: theme.spacing.unit,
+  },
+});
+
+const steps = ['Dados básicos', 'Potencial arquitetônico', 'Potêncial Paisagístico', 'Imagens'];
+
+function getStepContent(step) {
+  switch (step) {
+    case 0:
+      return <DadosBasicosForm />;
+    case 1:
+      return ;
+    case 2:
+      return ;
+    default:
+      throw new Error('Unknown step');
+  }
 }
 
-export default CadastroEspecie 
+class CadastroEspecie extends React.Component {
+  state = {
+    activeStep: 0,
+  };
+
+  handleNext = () => {
+    const { activeStep } = this.state;
+    this.setState({
+      activeStep: activeStep + 1,
+    });
+  };
+
+  handleBack = () => {
+    const { activeStep } = this.state;
+    this.setState({
+      activeStep: activeStep - 1,
+    });
+  };
+
+  handleReset = () => {
+    this.setState({
+      activeStep: 0,
+    });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { activeStep } = this.state;
+
+    return (
+      <React.Fragment>      
+        <main className={classes.layout}>
+          <Paper className={classes.paper}>
+            <Typography variant="display1" align="center">
+              Cadastro de Espécie
+            </Typography>
+            <Stepper activeStep={activeStep} className={classes.stepper}>
+              {steps.map(label => (
+                <Step key={label}>
+                  <StepLabel>{label}</StepLabel>
+                </Step>
+              ))}
+            </Stepper>
+            <React.Fragment>
+              {activeStep === steps.length ? (
+                <React.Fragment>
+                  <Typography variant="headline" gutterBottom>
+                    Espécie cadastrada com sucesso!
+                  </Typography>
+                </React.Fragment>
+              ) : (
+                <React.Fragment>
+                  {getStepContent(activeStep)}
+                  <div className={classes.buttons}>
+                    {activeStep !== 0 && (
+                      <Button onClick={this.handleBack} className={classes.button}>
+                        Voltar
+                      </Button>
+                    )}
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      onClick={this.handleNext}
+                      className={classes.button}
+                    >
+                      {activeStep === steps.length - 1 ? 'Salvar ' : 'Próximo'}
+                    </Button>
+                  </div>
+                </React.Fragment>
+              )}
+            </React.Fragment>
+          </Paper>
+        </main>
+      </React.Fragment>
+    );
+  }
+}
+
+CadastroEspecie.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(CadastroEspecie);
