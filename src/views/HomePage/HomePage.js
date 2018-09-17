04f4fbf0c823944ad755f519b545017eeb200a32
jose.filho <jose.goulart@acad.pucrs.br>
2018-09-17 19:08:27 -0300

    homepage

--- src/views/HomePage/HomePage.js
+++ src/views/HomePage/HomePage.js
@@ -1,18 +1,57 @@
 import React from "react";
-import "./styles.css";
+
 import { withRouter } from "react-router-dom";
+import Button from "@material-ui/core/Button";
 
 // Biblioteca de Componentes
 
 // Views
 import Page from "views/Page/Page";
-import { Button, Menu, MenuItem, IconButton } from "@material-ui/core";
+import { Menu, MenuItem, IconButton } from "@material-ui/core";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
+import Paper from "@material-ui/core/Paper";
+
+import Grid from "@material-ui/core/Grid";
 
 // Serviços
 import { listAll } from "services/user/user";
 
+import img from "./paisagem.jpeg"
+
+const styles = theme => ({
+  button: {
+    margin: theme.spacing.unit,
+    marginTop: theme.spacing.unit * 3
+  },
+  input: {
+    display: "none"
+  },
+
+  paper: {
+    marginTop: theme.spacing.unit * 3,
+    marginBottom: theme.spacing.unit * 3,
+    padding: theme.spacing.unit * 2,
+    [theme.breakpoints.up(600 + theme.spacing.unit * 3 * 2)]: {
+      marginTop: theme.spacing.unit * 3,
+      marginBottom: theme.spacing.unit * 3,
+      padding: theme.spacing.unit * 3
+    }
+  },
+
+  layout: {
+    width: "auto",
+    marginLeft: theme.spacing.unit * 2,
+    marginRight: theme.spacing.unit * 2,
+    [theme.breakpoints.up(900 + theme.spacing.unit * 2 * 2)]: {
+      width: 800,
+      marginLeft: "auto",
+      marginRight: "auto"
+    }
+  }
+});
+
 class HomePage extends Page {
+  
   authenticated = () => {
     return (
       <div
@@ -23,7 +62,7 @@ class HomePage extends Page {
         <Button
           variant="contained"
           size="large"
-          color="secondary"
+          color="primary"
           onClick={() => this.redirect("/cadastroEspecie")}
           className="clickable"
         >
@@ -34,16 +73,22 @@ class HomePage extends Page {
   };
 
   unauthenticated = () => {
+   
     return (
       <div
         style={{
-          paddingTop: "100px"
+          height: "93vh",
+          backgroundImage:`url(${img})`
         }}
       >
+       <Paper elevation={1}>
+                <Grid container spacing={60}>
+                  <Grid item xs={40}>
+                  </Grid>
         <Button
           variant="contained"
           size="large"
-          color="secondary"
+          color="primary"
           onClick={() => this.redirect("/login")}
           className="clickable"
         >
@@ -54,13 +99,18 @@ class HomePage extends Page {
         <Button
           variant="contained"
           size="large"
-          color="secondary"
-          onClick={() => this.redirect("/cadastroEspecie")}
+          color="primary"
+          onClick={() => this.redirect("/listagemEspecie")}
           className="clickable"
         >
-          Cadastrar Especie
+          Listagem de Especie
         </Button>
+       
+     
+      </Grid>
+      </Paper>
       </div>
+        
     );
   };
 }
