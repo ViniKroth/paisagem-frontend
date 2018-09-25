f00d19bdb13ece52cc344741d7ba27758034633b
Felipe L. Guella <felipe.guella@acad.pucrs.br>
2018-09-25 23:07:56 +0000

    Coloquei uma foto genérica (tirado do detalhe de especie) e outras mudanças pequenas

--- src/views/ListagemEspecie/ListagemEspecie.js
+++ src/views/ListagemEspecie/ListagemEspecie.js
@@ -11,6 +11,8 @@ import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 import Page from "views/Page/Page.js";
 import Paper from "@material-ui/core/Paper";
+import mate from "./mate.png";
+import araucaria from "./araucaria.jpg";
 
 const styles = theme => ({
   layout: {
@@ -47,6 +49,12 @@ const styles = theme => ({
   cardContent: {
     flexGrow: 1,
   },
+  buttonSee: {
+    width: '100%',
+    height: '30%',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
 
 });
 
@@ -58,9 +66,10 @@ class ListagemEspecie extends Page {
     this.state = {
         value: 0,
         nome_cientifico: 'Paenibacillus riograndensis',
-        nome_popular: {},
-        foto: '',
-        origem: '', // com icone
+        nome_popular: ['po', 'de'],
+        //foto: 'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22288%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20288%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_164edaf95ee%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_164edaf95ee%22%3E%3Crect%20width%3D%22288%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2296.32500076293945%22%20y%3D%22118.8%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
+        foto: araucaria,
+        origem: mate, // com icone
         porte: '',
         folhagem: '', // com icone
     };
@@ -98,23 +107,35 @@ class ListagemEspecie extends Page {
                 <Card className={classes.card}>
                   <CardMedia
                     className={classes.cardMedia}
-                    image="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22288%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20288%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_164edaf95ee%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_164edaf95ee%22%3E%3Crect%20width%3D%22288%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2296.32500076293945%22%20y%3D%22118.8%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" // eslint-disable-line max-len
-                    title="Image title"
+                    image= {this.state.foto} // eslint-disable-line max-len
+                    title= {this.state.nome_cientifico.italics()}
                   />
                   <CardContent className={classes.cardContent}>
                     <Typography gutterBottom variant="headline" component="h2"  >
-                      {this.state.nome_cientifico}
+                    <img
+                        src={this.state.origem}
+                        alt="nativa"
+                        height="40" width="40"
+                      />
+                      <i>
+                     {"\t"} {this.state.nome_cientifico} 
+                     </i>
                     </Typography>
-                    <Typography>
-                      This is a media card. You can use this section to describe the content.
+                    <Typography>                      
+                      <p>
+                      Nomes Populares: {this.state.nome_popular} 
+                      </p>                   
+                      <p>
+                      Porte: {this.state.porte}
+                      </p>                      
+                      <icon>
+                        {this.state.folhagem}
+                      </icon>
                     </Typography>
                   </CardContent>
                   <CardActions>
-                    <Button size="small" color="primary">
-                      View
-                    </Button>
-                    <Button size="small" color="primary">
-                      Edit
+                    <Button size="medium" color="primary" variant="contained" className={classes.buttonSee}>
+                      detalhes
                     </Button>
                   </CardActions>
                 </Card>
