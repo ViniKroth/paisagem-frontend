c7e9d3f19d806f6feea252aa97e857a21727d628
Felipe L. Guella <felipe.guella@acad.pucrs.br>
2018-09-28 19:24:57 +0000

    adicionado Chips com os nomes populares

--- src/views/ListagemEspecie/ListagemEspecie.js
+++ src/views/ListagemEspecie/ListagemEspecie.js
@@ -17,6 +17,7 @@ import perene from "./perene.png";
 import caduca from "./caduca.png";
 import araucaria from "./araucaria.jpg";
 import IconButton from '@material-ui/core/IconButton';
+import Chip from '@material-ui/core/Chip';
 import Filter from '@material-ui/icons/Filter';
 import { Icon } from '@material-ui/core';
 
@@ -65,7 +66,10 @@ const styles = theme => ({
     height:'10%',
      width:'25%',
     align: 'center'
-  }
+  },
+  chip: {
+    margin: theme.spacing.unit,
+  },
 
 });
 
@@ -77,7 +81,7 @@ class ListagemEspecie extends Page {
     this.state = {
         value: 0,
         nome_cientifico: 'Araucaria angustifolia',
-        nome_popular: ['po', 'de'],
+        nome_popular: ['pinheiro-brasileiro', 'pinheiro-do-paraná'],
         //foto: 'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22288%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20288%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_164edaf95ee%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_164edaf95ee%22%3E%3Crect%20width%3D%22288%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2296.32500076293945%22%20y%3D%22118.8%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
         foto: araucaria,
         origem: 'Nativa', // com icone
@@ -163,7 +167,7 @@ class ListagemEspecie extends Page {
                     </Typography>
                     <Typography>                      
                       <p>
-                      Nomes Populares: {this.state.nome_popular} 
+                      Nomes Populares: {this.state.nome_popular.map((nome) => <Chip label={nome} className={classes.chip} />)}
                       </p>                   
                       <p>
                       Porte: {this.state.porte}
