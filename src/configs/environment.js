c5f3645b65028df230fc3071f87927faaa519385
Cassio Trindade <cassio.trindade@pucrs.br>
2018-10-05 18:28:24 -0300

    inserido consolelog no envi

--- src/configs/environment.js
+++ src/configs/environment.js
@@ -1,3 +1,7 @@
 var env       = process.env.NODE_ENV || 'development'
 var environment    = require('./config.json')[env]
+
+console.log("MUITO MERDA"+ process.env.REACT_APP_API_URL)
+
 export default environment
+
