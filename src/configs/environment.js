d1cb69d3cb7892e239042d7b0243acf9db2c088b
Cassio Trindade <cassio.trindade@pucrs.br>
2018-10-06 15:02:52 -0300

    incluido o env production

--- src/configs/environment.js
+++ src/configs/environment.js
@@ -1,6 +1,6 @@
 var env       = process.env.NODE_ENV || 'development'
 console.log(env)
-var environment    = require('./config.json')[env]
+var environment    = require('./config.json')
 console.log(environment)
 export default environment
 
