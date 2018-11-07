9d6f76bb0c234259142ed6ff5cd944627ae52efb
aranha <paulo.aranha@acad.pucrs.br>
2018-11-07 17:33:56 -0200

    merge com a dev

--- src/views/ListagemFamilia/ListagemFamilia.js
+++ src/views/ListagemFamilia/ListagemFamilia.js
@@ -117,6 +117,20 @@ const styles = theme => ({
     chip: {
         margin: theme.spacing.unit,
     },
+    card: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
 });
 
 
